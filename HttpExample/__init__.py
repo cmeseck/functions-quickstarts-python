10e0636d3e09196ec55dd3239d25b93d58d6d3e3
cmeseck <162364749+cmeseck@users.noreply.github.com>
2025-03-13 22:06:27 -0500

    Update __init__.py

--- HttpExample/__init__.py
+++ HttpExample/__init__.py
@@ -4,4 +4,4 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-    return func.HttpResponse(str("YESSSIR!!!!))
+    return func.HttpResponse(str("YESSSIR!!!!"))
